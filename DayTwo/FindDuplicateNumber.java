dd6c5e83547b7334fa486c0b407f3f747a686052
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-08-15 17:49:53 +0530

    Added two more questions of DayTwo: MergeSortedArray and FindDuplicateNumber

---
+++ DayTwo/FindDuplicateNumber.java
@@ -0,0 +1,22 @@
+package DayTwo;
+
+import java.util.Arrays;
+
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+ */
+
+public class FindDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1])
+                return nums[i];
+        }
+        return 0;
+    }
+}
